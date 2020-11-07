720f161cb78d9afb94968cd86ac578d6e878e17d
JamesVanWaza <jgacuca@gmail.com>
2020-11-06 19:41:51 -0500

    Updated to Gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,82 @@
-var gulp = require('gulp'),
-    webserver = require('gulp-webserver');
+// var gulp = require('gulp'),
+//     webserver = require('gulp-webserver');
 
-var builds = 'builds/bootstrap';
+// var builds = 'builds/bootstrap';
 
-gulp.task('js', function() {
-  return gulp.src(builds + '/js/myscript.js')
-});
+// gulp.task('js', function() {
+//     return gulp.src(builds + '/js/myscript.js');
+// });
 
-gulp.task('html', function() {
-  gulp.src(builds + '/**/*.html');
-});
+// gulp.task('html', function() {
+//     gulp.src(builds + '/**/*.html');
+// });
 
-gulp.task('css', function() {
-    gulp.src(builds + '/css/*.css');
-});
+// gulp.task('css', function() {
+//     gulp.src(builds + '/css/*.css');
+// });
 
-gulp.task('watch', function() {
-    gulp.watch(builds + '/js/**/*', ['js']);
-    gulp.watch(builds + '/css/**/*.css', ['css']);
-    gulp.watch([builds + '/**/*.html'], ['html']);
-});
+// gulp.task('watch', function() {
+//     gulp.watch(builds + '/js/**/*', ['js']);
+//     gulp.watch(builds + '/css/**/*.css', ['css']);
+//     gulp.watch([builds + '/**/*.html'], ['html']);
+// });
 
-gulp.task('webserver', function() {
-    gulp.src(builds + '/')
-        .pipe(webserver({
-            port: 3000,
-            livereload: true,
-            livereloadport: 8283,
-            open: true
-        }));
-});
+// gulp.task('webserver', function() {
+//     gulp.src(builds + '/')
+//         .pipe(webserver({
+//             port: 3000,
+//             livereload: true,
+//             livereloadport: 8283,
+//             open: true
+//         }));
+// });
 
-gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
+// gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
+
+const gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    sourcemaps = require("gulp-sourcemaps"),
+    browserSync = require("browser-sync").create(),
+    source = "./process/",
+    dest = "./builds/bootstrap/";
+
+sass.compiler = require("node-sass");
+
+function html() {
+    return gulp.src(dest + "**/*.html");
+}
+
+function js() {
+    return gulp.src(dest + "**/*.js");
+}
+
+// function styles() {
+//     return gulp.src(source + "sass/style.scss")
+//         .pipe(sourcemaps.init()).pipe(sass({
+//             sourcemap: true,
+//             style: "compressed"
+//         }).on("error", sass.logError)).pipe(gulp.dest(dest + "css"));
+// }
+
+function watch() {
+    gulp.watch(dest + "js/**/*.js", js).on("change", browserSync.reload);
+    // gulp.watch(source + "sass/**/*", styles).on("change", browserSync.reload);
+    gulp.watch(dest + "index.html", html).on("change", browserSync.reload);
+}
+
+function server() {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: dest
+        }
+    });
+
+    // gulp.watch(source + "sass/**/*.scss", styles).on("change", browserSync.reload);
+    gulp.watch(dest + "js/**/*.js", js).on("change", browserSync.reload);
+    gulp.watch(dest + "index.html", html).on("change", browserSync.reload);
+}
+
+var build = gulp.series(gulp.parallel(js, html), server, watch);
+
+gulp.task("default", build);
